4c6ffb8c6ec1d13458d4381712efe8fc7ff46b31
weustace <weustace@eustacesheen.me.uk>
2014-03-08 14:34:16 +0000

    Fixed HYT271 code - now working well. Consider processing data afterwards to save on storage overhead + flash space + RAM + power when using floats?

--- modules/HYT271/first_interpretation/first_interpretation.ino
+++ modules/HYT271/first_interpretation/first_interpretation.ino
@@ -8,50 +8,61 @@ I have based this roughly on the C-language sketch found at ist-ag.com (see read
 Changelog:
 
 14-3-6: Created rough outline. Have not tested, but compiles well to ~3kb.
-
+14-3-8: Fixed numerous errors. Now working well, but binary size 8.7kb w/ serial strings. 
 */
 #define hytAddr 0x28
 float temp, hum;
+byte data[4];
 
 
 void setup(){
   Wire.begin();//Join i2c bus
   Serial.begin(9600);//set up serial for debugging
-  
+  send_HYT_MR();//We'll see if we can eliminate this delay in the final copy.  
   
 }
 
-void loop(){}
+void loop(){
+
+  read_HYT();
+    send_HYT_MR();
+  Serial.println("===================");
+  handle_HYT_data();
+Serial.println(hum);Serial.println(temp);
+delay(500);
+
+}
 
 void send_HYT_MR(){//send the instruction to start taking a measurement
 //needs a 60ms delay before reading
   Wire.beginTransmission(hytAddr);
-  Wire.write(0);//I !!THINK!! this can be basically anything.
   Wire.endTransmission();
 }
 
-void read_HYT_MR(byte data[5]){
-  Wire.requestFrom(hytAddr,5);//V Important - we don't wait for wire available, 
+void read_HYT(){
+  
+  Wire.requestFrom(hytAddr,4);//V Important - we don't wait for wire available, 
   //in case this causes a hang.
-  while(Wire.available()){
-    for(int i = 0;i<5;i++){
+    for(byte i = 0;i<4;i++){
       data[i] = Wire.read();//read the expected five bytes into an array.
     }
+    
+    Wire.endTransmission();
+}
+
+void handle_HYT_data(){
+  int rawH = ((data[0] << 8) & 0x3FFF) | data[1]; 
+
+  
+  int rawT = data[2] << 6;
+  rawT = rawT | (data[3] >> 2);
+  if(rawT < 0x3FFF && rawH < 0x3FFF){
+    temp = ((float)(rawT) * 165.0 / 16384.0) - 40.0;
+    hum = (float)rawH * 100.0 / 16384.0;
+    
+  }
+  else{
+    Serial.println("Data out of range.");
   }
 }
 
-boolean handle_HYT_data(byte data[5]){
-  
-  if(((data[1] & 0x40)>>6) == 0){//if the data is new (status bits)
-    unsigned int temp_raw = (data[3] << 8) | (data[4] >> 2);
-    
-    unsigned int hum_raw = ((data[1] & 0x3F)<<8) | data[2];
-    
-    if (temp_raw < 0x3FFF && hum_raw < 0x3FFF){//if values within normal limits
-      temp = ((float)(temp_raw)*165.0F / 16383.0F) - 40.0F;
-      hum = (float)hum_raw * 100.0F / 16383.0F;
-    }
-    //Otherwise, something is kaputt.
-    
-  }
-}
