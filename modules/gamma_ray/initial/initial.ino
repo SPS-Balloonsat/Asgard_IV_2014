89c199539801d787c674c97f947c780dcea37254
William Eustace <weustace@eustacesheen.me.uk>
2014-03-13 21:36:20 +0000

    Added gamma photon counting code. NB this is very much subject to testing/changing.

---
+++ modules/gamma_ray/initial/initial.ino
@@ -0,0 +1,32 @@
+/*
+SPS Balloonsat 2014
+Gamma Ray Photon pulse counter
+Changelog:
+2014-3-13: Created initial file, added rough code. As yet untested.
+*/
+#define gammaIntPin 5
+//value arbitrary and to be changed
+volatile unsigned int photonHits;
+unsigned long sampleTime;
+
+void setup(){
+  Serial.begin(9600);
+  while(!Serial.available());
+  pinMode(gammaIntPin, INPUT);//We don't want pull-up resistors - these
+  //should be on the comparator chip, I think. Check?
+  attachInterrupt(gammaIntPin, incGamma, RISING);
+  sampleTime = millis();//sample timer
+}
+
+void loop(){
+  if(((millis() - sampleTime)) > 200){
+    Serial.println((float)(photonHits / (millis() - sampleTime)));
+    Serial.println("==================");
+    photonHits = 0;
+    sampleTime = millis();
+  }  
+}
+
+void incGamma(){
+  photonHits++;//ISR routine to be kept as short as possible
+}
